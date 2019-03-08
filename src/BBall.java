0d80d51bfd1e856acdf937e91fd75252aef20665
Raymond <lycheesun@hotmail.com>
2019-03-08 16:26:51 -0500

    Update

--- src/BBall.java
+++ src/BBall.java
@@ -3,5 +3,5 @@ import java.awt.*;
 public class BBall{
 	protected Point posn;
 	protected Color color;
-
+	protected int ballNumber;
 }
---
+++ src/CueBall.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+
+public class CueBall extends BBall{
+    public CueBall(){
+        super();
+        super.color = Color.WHITE;
+        super.posn = 
+    }
+}
